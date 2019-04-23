639a5dee9ff20fca386613b91ed5d5ffa6bdccdd
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-23 12:01:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+  two = 2
 }
 
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  two = 2
+  
 }
