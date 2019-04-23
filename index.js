d85d9c5ccbc9e49d343de94352573a1c07999323
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-23 12:02:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,16 +5,10 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   return animal
-  two = 2
 }
 
 function add2(n) {
   return n + two
-
-  // Feel free to move things around!
-  
+  two = 2
 }
